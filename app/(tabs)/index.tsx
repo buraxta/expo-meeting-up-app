13340f9d3d691b8f7152096f685c13342d6084f4
buraxta <burakgulluler@gmail.com>
2024-10-13 23:53:40 +0300

    Add event listing feature with EventListItem component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
 import { Stack } from 'expo-router';
+import { View } from 'react-native';
+
+import events from '~/assets/events.json';
+import EventListItem from '~/components/EventListItem';
 
 export default function Events() {
   return (
-    <>
+    <View>
       <Stack.Screen options={{ title: 'Events' }} />
-    </>
+
+      {/* Event list item */}
+      <EventListItem event={events[0]} />
+      <EventListItem event={events[1]} />
+      <EventListItem event={events[2]} />
+    </View>
   );
 }
